1186397da66cfe65d8d1fa2e6fcd8ee7a2971fd1
Sihle <msyhlez@gmail.com>
2014-05-10 10:53:58 +0200

    Updated from Sat class.

--- src/Chance.java
+++ src/Chance.java
@@ -1,19 +1,14 @@
 public class Chance {
-    private final int number;
-    private final int nChances;
-    public Chance(int number, int nChances) {
-        this.number = number;
-        this.nChances = nChances;
+    private final int occurrence;
+    private final int totalNumberOfOccurrences;
+    public Chance(int occurrence, int totalNumberOfOccurrences) {
+        this.occurrence = occurrence;
+        this.totalNumberOfOccurrences = totalNumberOfOccurrences;
     }
-
-    public int probability() {
-        return number * nChances;
-    }
-
     @Override
     public boolean equals(Object obj) {
         Chance chanceObj = (Chance) obj;
-        return (chanceObj.number == this.number && chanceObj.nChances == this.nChances);
+        return (chanceObj.occurrence == this.occurrence && chanceObj.totalNumberOfOccurrences == this.totalNumberOfOccurrences);
     }
 
 }
